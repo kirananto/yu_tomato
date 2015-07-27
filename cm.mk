019cf561cd0050eea46206ac452c805f0d4bdebd
vibhu0009 <cvibhu0009@gmail.com>
2015-07-27 12:43:46 +0000

    stable one

--- cm.mk
+++ cm.mk
@@ -14,10 +14,10 @@
 
 $(call inherit-product, device/yu/tomato/full_tomato.mk)
 
-# Inherit some common exodus stuff.
-$(call inherit-product, vendor/exodus/config/common_full_phone.mk)
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_NAME := exodus_tomato
+PRODUCT_NAME := cm_tomato
 BOARD_VENDOR := yu
 PRODUCT_DEVICE := tomato
 
